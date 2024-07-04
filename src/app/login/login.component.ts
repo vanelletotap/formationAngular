95448a53ed1b7bf28dbf5be2e4d2f432e9faf2ad
sidof <69512885+sidofdountio@users.noreply.github.com>
2024-07-04 14:28:21 +0100

    Update login.component.ts
    J'ai simule l'ajout d'ne method login. qui s'appele onLogin()
    je vais cable ca avec un button et un event de type click dans le html

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  constructor(privater router:Router){}
+
+  onLogin(){
+     this.router.navigate(["/accueil"])
+  }
 
 }
